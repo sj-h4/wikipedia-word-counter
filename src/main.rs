7e0275ea050bec619e04bbff550583b633b63ad2
sj <64534668+sj-h4@users.noreply.github.com>
2023-01-12 17:36:39 +0900

    Add tokenizer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use lindera::tokenizer::{Token, Tokenizer};
+use std::{
+    env,
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+fn tokenize(target_text: &String) -> Vec<Token> {
+    let tokenizer = Tokenizer::new().unwrap();
+    let tokens = tokenizer
+        .tokenize(&target_text)
+        .expect("failed to tokenize");
+    return tokens;
+}
+
+#[test]
+fn test_tokenize() {
+    let target_text = "ナイフで木を削った。".to_string();
+    let tokens = tokenize(&target_text);
+    for token in &tokens {
+        println!("{:?}", token.detail);
+    }
+    assert_eq!(tokens.len(), 4);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = env::args().collect::<Vec<String>>();
+    let file_path = &args[1];
+    let file = File::open(file_path).unwrap();
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let _tokens = tokenize(&line);
+    }
 }
