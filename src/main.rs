4e44b13c0e582a3866d412865bd7337546dc6e86
sj <64534668+sj-h4@users.noreply.github.com>
2023-01-14 10:14:08 +0900

    Add word counter

--- src/main.rs
+++ src/main.rs
@@ -16,12 +16,26 @@ fn tokenize(target_text: &String) -> Vec<Token> {
 
 #[test]
 fn test_tokenize() {
-    let target_text = "ナイフで木を削った。".to_string();
+    let target_text = "今日は晴れだ。".to_string();
     let tokens = tokenize(&target_text);
     for token in &tokens {
         println!("{:?}", token.detail);
     }
-    assert_eq!(tokens.len(), 4);
+    assert_eq!(tokens.len(), 5);
+}
+
+#[test]
+fn test_word_count() {
+    let mut word_counter = HashMap::new();
+    let target_text = "今日は晴れだ。今日は雨が降らない。明日も晴れだ。".to_string();
+    let tokens = tokenize(&target_text);
+    for token in &tokens {
+        let word = token.text;
+        let entry = word.to_string();
+        word_counter.entry(entry).and_modify(|counter| *counter += 1).or_insert(1);
+    }
+    assert_eq!(word_counter.get("今日").unwrap(), &2);
+    assert_eq!(word_counter.len(), 11)
 }
 
 fn main() {
@@ -30,13 +44,15 @@ fn main() {
     let file_path = &args[1];
     let file = File::open(file_path).unwrap();
     let reader = BufReader::new(file);
-    for line in reader.lines() {
-        let line = line.unwrap();
+    let lines = reader.lines();
+    for line in lines {
+        let line = line.unwrap().clone();
         let tokens = tokenize(&line);
         for token in &tokens {
             let word = token.text;
-            let count = word_counter.entry(word).or_insert(0);
-            *count += 1;
+            let entry = word.to_string();
+            word_counter.entry(entry).and_modify(|counter| *counter += 1).or_insert(1);
         }
     }
+    println!("{:?}", word_counter.len());
 }
