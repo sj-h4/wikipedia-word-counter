dc47471d8fa121693537d0296345fa39210a2a89
sj <64534668+sj-h4@users.noreply.github.com>
2023-01-15 16:27:55 +0900

    Fix

--- script/format_data.py
+++ script/format_data.py
@@ -1,9 +1,10 @@
 import json
 
-file_path = 'output/result.json'
+file_path = 'output/result1.json'
 sorted_file_path = 'output/sorted_result.json'
 with open(file_path, 'r') as f:
     data = json.load(f)
-    sorted_data = sorted(data.items(), key=lambda x: x[1], reverse=True)
+    sorted_data_list = sorted(data.items(), key=lambda x: x[1], reverse=True)
+    sorted_data = dict((k, v) for k, v in sorted_data_list)
     with open(sorted_file_path, 'w') as f:
         json.dump(sorted_data, f, indent=2, ensure_ascii=False)
