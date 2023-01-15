95ffcce5c42cb0a57096c15374323954394b6434
sj <64534668+sj-h4@users.noreply.github.com>
2023-01-15 13:55:08 +0900

    Update lindera

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
+use lindera::{tokenizer::{DictionaryConfig, TokenizerConfig, Tokenizer}, DictionaryKind, mode::Mode, Token};
 use rayon::prelude::*;
-use lindera::tokenizer::{Token, Tokenizer};
 use std::{
     collections::HashMap,
     env,
@@ -7,6 +7,20 @@ use std::{
     io::Write,
 };
 
+fn get_tokenizer() -> Tokenizer {
+    let dictionary = DictionaryConfig {
+        kind: Some(DictionaryKind::IPADIC),
+        path: None,
+    };
+
+    let config = TokenizerConfig {
+        dictionary,
+        user_dictionary: None,
+        mode: Mode::Normal,
+        with_details:false,
+    };
+    return Tokenizer::from_config(config).unwrap();
+}
 fn tokenize<'a>(tokenizer: &'a mut Tokenizer, target_text: &'a str) -> Vec<Token<'a>> {
     let tokens = tokenizer
         .tokenize(target_text)
@@ -16,24 +30,23 @@ fn tokenize<'a>(tokenizer: &'a mut Tokenizer, target_text: &'a str) -> Vec<Token
 
 #[test]
 fn test_tokenize() {
-    let mut tokenizer = Tokenizer::new().unwrap();
+    let mut tokenizer = get_tokenizer();
     let target_text = "今日は晴れだ。".to_string();
     let tokens = tokenize(&mut tokenizer, &target_text);
     for token in &tokens {
-        println!("{:?}", token.detail);
+        println!("{:?}", token.text);
     }
     assert_eq!(tokens.len(), 5);
 }
 
 #[test]
 fn test_word_count() {
-    let mut tokenizer = Tokenizer::new().unwrap();
+    let mut tokenizer = get_tokenizer();
     let mut word_counter = HashMap::new();
     let target_text = "今日は晴れだ。今日は雨が降らない。明日も晴れだ。".to_string();
     let tokens = tokenize(&mut tokenizer, &target_text);
     for token in &tokens {
-        let word = token.text;
-        let entry = word.to_string();
+        let entry= token.text.to_string();
         word_counter.entry(entry).and_modify(|counter| *counter += 1).or_insert(1);
     }
     assert_eq!(word_counter.get("今日").unwrap(), &2);
@@ -51,14 +64,14 @@ fn count_word(tokenizer: &mut Tokenizer, text: &str) -> HashMap<String, i32> {
     let tokens = tokenize(tokenizer, text);
     let mut word_counter = HashMap::new();
     for token in &tokens {
-        let word = token.text;
-        let entry = word.to_string();
+        let entry = token.text.to_string();
         word_counter.entry(entry).and_modify(|counter| *counter += 1).or_insert(1);
     }
     return word_counter;
 }
 
 fn main() {
+    
     let mut word_counter = HashMap::new();
     let args = env::args().collect::<Vec<String>>();
     let file_path = &args[1];
@@ -66,7 +79,7 @@ fn main() {
 
     let contents = fs::read_to_string(file_path).expect("Something went wrong reading the file");
     let count = contents.par_lines().map_init(
-        || Tokenizer::new().unwrap(),
+        || get_tokenizer(),
         | tokenizer, line | count_word(tokenizer, line));
     let map_list: Vec<HashMap<String, i32>> = count.collect();
     for map in map_list {
